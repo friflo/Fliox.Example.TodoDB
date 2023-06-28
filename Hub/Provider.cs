5d42167cdf92bba7a969554731ca2bf897c28a3d
Ullrich Praetz <ullrich@praetz.de>
2023-06-28 14:47:50 +0200

    Provider - add MemoryDatabase()

--- Hub/Provider.cs
+++ Hub/Provider.cs
@@ -10,7 +10,9 @@ namespace TodoHub;
 
 public static class Provider
 {
-    private static readonly Dictionary<string,string> ConnectionStrings = new Dictionary<string,string>(){
+    private static readonly Dictionary<string,string> ConnectionStrings = new() {
+        { "file",       "../Test/DB/main_db" }, // records stored in folder: main_db/jobs
+        { "sqlite",     "todo_db.sqlite3" },
         { "mysql",      "Server=localhost;Port=3306;User ID=root;Password=YOURPASSWORD;Database=todo_db;Application Name=Fliox" },
         { "mariadb",    "Server=localhost;Port=3307;User ID=root;Password=YOURPASSWORD;Database=todo_db" },
         { "postgres",   "Host=localhost;Username=postgres;Password=YOURPASSWORD;Database=todo_db;Application Name=Fliox" },
@@ -21,12 +23,13 @@ public static class Provider
         ConnectionStrings.TryGetValue(provider, out var config);
         switch (provider) {
             case null:
-            case "file":      return new FileDatabase      (db, "../Test/DB/main_db", schema); // records stored in 'main_db/jobs'
-            case "sqlite":    return new SQLiteDatabase    (db, "todo_db.sqlite3", schema);  // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.SQLite/
-            case "mysql":     return new MySQLDatabase     (db, config, schema);  // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.MySQL/
-            case "mariadb":   return new MariaDBDatabase   (db, config, schema);  // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.MySQL/
-            case "postgres":  return new PostgreSQLDatabase(db, config, schema);  // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.PostgreSQL/
-            case "sqlserver": return new SQLServerDatabase (db, config, schema);  // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.SQLServer/
+            case "memory":    return new MemoryDatabase     (db, schema);
+            case "file":      return new FileDatabase       (db, config, schema);
+            case "sqlite":    return new SQLiteDatabase     (db, config, schema); // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.SQLite/
+            case "mysql":     return new MySQLDatabase      (db, config, schema); // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.MySQL/
+            case "mariadb":   return new MariaDBDatabase    (db, config, schema); // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.MySQL/
+            case "postgres":  return new PostgreSQLDatabase (db, config, schema); // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.PostgreSQL/
+            case "sqlserver": return new SQLServerDatabase  (db, config, schema); // https://www.nuget.org/packages/Friflo.Json.Fliox.Hub.SQLServer/
             default:          throw  new InvalidOperationException($"unknown provider: {provider}");
         }
     }
